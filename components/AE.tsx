1cfb603370759e10444532a5dfc45bf503e1b94e
Rieranthony <rieraanthony13@gmail.com>
2022-06-06 12:13:54 +0100

    feat: added cool animations and interactions

--- components/AE.tsx
+++ components/AE.tsx
@@ -1,4 +1,5 @@
 import { AutonomousEntity, AutonomousEntities } from "../data";
+import { motion } from "framer-motion";
 
 type AEProps = {
   onGoToAE: (id: string) => void;
@@ -14,23 +15,39 @@ export function AE({
   level,
   currentStackLength
 }: AEProps) {
-  const opacity = 1 + (level - currentStackLength + 1) / 10;
-  const offset = level * 8;
+  const position = currentStackLength - 1 - level;
+  const opacity = 1 - position / 10;
+  const offset = position * (8 + position);
   const dynamicStyles = {
-    left: `${offset}px`,
-    right: `-${offset}px`,
-    top: `${offset}px`,
-    zIndex: offset,
-    opacity
-  };
+    zIndex: level,
+    position: position === 0 ? "relative" : "absolute"
+  } as const;
+
+  if (position > 8) {
+    return;
+  }
 
   return (
-    <div
-      className={`bg-dp-00  flex flex-col p-4 border border-dp-medium-emphasis hover:border-dp-normal rounded-lg transition-colors absolute`}
+    <motion.div
+      drag
+      dragElastic={0.2}
+      whileHover={{ scale: 1.05, opacity: 1 }}
+      initial={{ opacity: 0.5, top: 300 }}
+      animate={{
+        opacity,
+        left: offset,
+        right: offset,
+        top: -offset
+      }}
+      exit={{ opacity: 0 }}
+      layout
+      className={`bg-dp-00 flex flex-col justify-between p-4 border border-dp-medium-emphasis hover:border-dp-normal rounded-lg transition-colors md:min-h-[360px]`}
       style={dynamicStyles}
     >
-      <p className="text-xl mt-4 max-w-full">{name}</p>
-      <p className="text-base mt-4 max-w-full">{description}</p>
+      <div>
+        <p className="text-xl mt-4 max-w-full">{name}</p>
+        <p className="text-base mt-4 max-w-full">{description}</p>
+      </div>
 
       <div className="grid gap-4 grid-flow-col grid-cols-2 mt-8">
         {relationships.map((AEId, i) => {
@@ -51,6 +68,6 @@ export function AE({
           );
         })}
       </div>
-    </div>
+    </motion.div>
   );
 }
